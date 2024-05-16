f3a2671739d770f9b92c4e11f75e40f35d5d10de
jedschlo14 <jedschloesser@gmail.com>
2024-05-15 19:35:28 -0500

    revised styles

--- client/src/components/timer/Timer.styles.js
+++ client/src/components/timer/Timer.styles.js
@@ -4,14 +4,13 @@ import { Button } from 'assets/styles/Common.styles';
 export const TimerWrapper = styled.div`
     grid-row: 1;
     grid-column: 1;
-    display: grid;
-    grid-template-rows: 1fr;
-    grid-template-columns: repeat(9, minmax(0, 1fr));
-    padding: 0.5rem;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 `;
 
 export const Digits = styled.span`
-    grid-column: 0;
     font-family: 'Roboto Mono', monospace;
     font-size: 3rem;
     font-weight: 600;
@@ -20,7 +19,8 @@ export const Digits = styled.span`
 `;
 
 export const PauseButton = styled(Button)`
-    grid-column: 8 / 10;
+    width: 8rem;
+    height: 3rem;
     border-width: 0.25rem;
     border-style: solid;
     font-size: 1.25rem;
